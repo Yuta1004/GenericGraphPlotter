214efb41d3ebd0a997639d161073e1d4196ebf53
NakagamiYuta <guguru0014@gmail.com>
2019-10-17 15:09:30 +0900

    [add] strtonメソッド作成

--- ESCompiler/ExprScriptCompiler.java
+++ ESCompiler/ExprScriptCompiler.java
@@ -3,6 +3,7 @@ package ESCompiler;
 public class ExprScriptCompiler {
 
     private String script;
+    private Node root;
 
     public ExprScriptCompiler() {
         this.setScript("");
@@ -16,4 +17,24 @@ public class ExprScriptCompiler {
         this.script = script;
     }
 
+    public void compile() {
+        this.root = new Node();
+    }
+
+    private void compilerExpr() {
+        
+    }
+
+    /* strton : 文字列から数字を読み取る */
+    private double strton(String target) {
+        double num = 0;
+        for(int idx = 0; idx < target.length(); ++ idx) {
+            if('0' <= target.charAt(idx) && target.charAt(idx) <= '9') {
+                num = num * 10 + target.charAt(idx) - '0';
+            } else {
+                break;
+            }
+        }
+        return num;
+    }
 }
