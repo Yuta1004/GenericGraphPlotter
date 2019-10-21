7d2a35adc5df1a05e76a466b8a2209be14ecba69
NakagamiYuta <guguru0014@gmail.com>
2019-10-21 23:44:59 +0900

    [change] Math.PI, Math.Eを使用するようにした

--- ESParser/ExprParser.java
+++ ESParser/ExprParser.java
@@ -11,8 +11,8 @@ public class ExprParser {
     public ExprParser(String expr) {
         this.expr = expr;
         this.var = new HashMap<String, Double>();
-        this.var.put("PI", 3.141592653589793);
-        this.var.put("E", 2.718281828459045);
+        this.var.put("PI", Math.PI);
+        this.var.put("E", Math.E);
     }
 
     public void compile() {
