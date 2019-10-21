c584cb3f88c177b38339d268b4c249aa63ce7a3f
NakagamiYuta <guguru0014@gmail.com>
2019-10-22 00:19:36 +0900

    [add] 変数定義をパースできるようにした

--- ESParser/ScriptParser.java
+++ ESParser/ScriptParser.java
@@ -8,10 +8,30 @@ public class ScriptParser {
     private Node root;
     private HashMap<String, Double> var;
 
-    public ScriptCompiler(String script) {
+    public ScriptParser(String script) {
         this.script = script;
         this.var = new HashMap<String, Double>();
     }
 
-    public void compile() { }
+    public void parse() {
+        for(String line: script.split("\n")) {
+            line = skipSpace(line);
+
+            // 変数定義
+            if(line.startsWith("var")) {
+                line = line.substring(3, line.length());
+                for(String varName: line.split(",")) {
+                    var.put(varName.replace(" ", ""), 0.0);
+                }
+            }
+        }
+    }
+
+    /* skipSpace : 式先頭にある空白を読み飛ばす*/
+    private String skipSpace(String target) {
+        while(target.length() > 0 && target.charAt(0) == ' ') {
+            target = target.substring(1, target.length());
+        }
+        return target;
+    }
 }
