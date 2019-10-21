fb549a2672bcb75eb389c8047c031d02896d1137
NakagamiYuta <guguru0014@gmail.com>
2019-10-22 01:54:00 +0900

    [add] loop, endのテストを書いた

--- ESParser/ScriptParserTest.java
+++ ESParser/ScriptParserTest.java
@@ -6,6 +6,8 @@ class ScriptParserTest {
         test1();
         test2();
         test3();
+        test4();
+        test5();
     }
 
     private static void test1() {
@@ -29,6 +31,37 @@ class ScriptParserTest {
             "b = 20",
             "c = a + b",
             "plot sin(a) + cos(b)",
+        };
+        ScriptParser parser = new ScriptParser(String.join("\n", script));
+        parser.parse();
+    }
+
+    private static void test4() {
+        String script[] = {
+            "var cnt, x, y",
+            "cnt = 0",
+            "loop: cnt < 10",
+            "   x = cnt * 10",
+            "   y = cnt * 5",
+            "   cnt = cnt + 1",
+            "end",
+        };
+        ScriptParser parser = new ScriptParser(String.join("\n", script));
+        parser.parse();
+    }
+
+    private static void test5() {
+        String script[] = {
+            "var cntA, cntB, x",
+            "cntA = 0",
+            "loop: cntA < 5",
+            "   cntB = 0",
+            "   loop: cntB < 3",
+            "       x = cntA*10 + cntB",
+            "       cntB = cntB + 1",
+            "   end",
+            "   cntA = cntA + 1",
+            "end",
         };
         ScriptParser parser = new ScriptParser(String.join("\n", script));
         parser.parse();
