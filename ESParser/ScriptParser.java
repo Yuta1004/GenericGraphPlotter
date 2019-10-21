6d4321d69cf71dea2de9d58b3be287d50f908af9
NakagamiYuta <guguru0014@gmail.com>
2019-10-22 00:05:32 +0900

    [add] ScriptParserに変数管理用の変数を追加

--- ESParser/ScriptParser.java
+++ ESParser/ScriptParser.java
@@ -1,12 +1,16 @@
 package ESParser;
 
+import java.util.HashMap;
+
 public class ScriptParser {
 
     private String script;
     private Node root;
+    private HashMap<String, Double> var;
 
     public ScriptCompiler(String script) {
         this.script = script;
+        this.var = new HashMap<String, Double>();
     }
 
     public void compile() { }
