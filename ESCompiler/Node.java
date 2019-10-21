1ff22febce25c95fba6a2b4573e27ba02e50e152
NakagamiYuta <guguru0014@gmail.com>
2019-10-21 14:11:28 +0900

    [add] Nodeクラス定義

--- ESCompiler/Node.java
+++ ESCompiler/Node.java
@@ -3,5 +3,12 @@ package ESCompiler;
 public class Node {
     public Node left, right;
     public double value;
-    public NodeKind kind;
+    public int kind;
+
+    public Node(Node left, Node right, double value, int kind){
+        this.left = left;
+        this.right = right;
+        this.value = value;
+        this.kind = kind;
+    }
 }
