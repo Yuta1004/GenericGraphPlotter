ca8a39a416515907753db474156941391003347c
NakagamiYuta <guguru0014@gmail.com>
2019-10-23 21:33:00 +0900

    [add] 背景描画部分を書いた

--- Main.java
+++ Main.java
@@ -5,11 +5,20 @@ import java.applet.*;
 
 public class Main extends Applet {
 
-    public void init() {
+    private int width, height;
+    private Color white;
 
+    public void init() {
+        // for applet
+        width = 1200;
+        height = 800;
+        white = new Color(255, 255, 255);
     }
 
     public void paint(Graphics g) {
+        // background
+        g.setColor(white);
+        g.drawRect(0, 0, width, height);
     }
 
 }
