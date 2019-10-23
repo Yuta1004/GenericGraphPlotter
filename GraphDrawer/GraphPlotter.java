deb6194b1a9a98454fee6941c28135ca1c4c991a
NakagamiYuta <guguru0014@gmail.com>
2019-10-23 23:48:44 +0900

    [add] コメント追加(GraphPlotter)

--- GraphDrawer/GraphPlotter.java
+++ GraphDrawer/GraphPlotter.java
@@ -10,6 +10,7 @@ class GraphPlotter {
     private double xArray[], yArray[];
     private Color color;
 
+    /* コンストラクタ */
     public GraphPlotter(int oX, int oY, double sX, double sY) {
         originX = oX;
         originY = oY;
@@ -18,23 +19,28 @@ class GraphPlotter {
         color = new Color(255, 100, 100);
     }
 
+    /* setColor(Color c) : 色指定(Colorインスタンス) */
     public void setColor(Color c) {
         color = c;
     }
 
+    /* setColor(int r, int g, int b) : 色指定(RGB) */
     public void setColor(int r, int g, int b) {
         color = new Color(r, g, b);
     }
 
+    /* setStroke : 線の太さ指定 */
     public void setStroke(int s) {
         stroke = s;
     }
 
+    /* setGraph : グラフ設定 */
     public void setGraph(double xArray[], double yArray[]) {
         this.xArray = xArray;
         this.yArray = yArray;
     }
 
+    /* plot : グラフ描画 */
     public void plot(Graphics2D g) {
         int x0, y0, x1, y1;
         int size = Math.min(xArray.length, yArray.length);
@@ -50,10 +56,12 @@ class GraphPlotter {
         }
     }
 
+    /* グラフ上の座標を描画用の座標に変換する(x) */
     private int g2aX(double x) {
         return originX + (int)(x*scaleX);
     }
 
+    /* グラフ上の座標を描画用の座標に変換する(y) */
     private int g2aY(double y) {
         return originY - (int)(y*scaleY);
     }
