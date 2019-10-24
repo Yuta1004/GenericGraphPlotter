754312e7404576bd42451b5182e401d917f9bc66
NakagamiYuta <guguru0014@gmail.com>
2019-10-24 21:30:01 +0900

    [change] min, max変数追加

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main extends Applet {
     private int width, height;
 
     // グラフ描画用
-    private int originX, originY;
+    private int originX, originY, min, max;
     private double scaleX, scaleY, dx, dy;
     String script;
 
@@ -26,6 +26,8 @@ public class Main extends Applet {
         originY = 670;
         scaleX = 80.0;
         scaleY = 80.0;
+        min = 0;
+        max = 10;
         dx = 0.05;
         dy = 1.0;
         script = "var a, b, c, d\na = 1.2\nb = -1.2\nc = 1.2\nd = 0.1\nplot sin(a*x + b) + cos(c*x + d) + 2";
@@ -43,7 +45,7 @@ public class Main extends Applet {
         sp.parse();
 
         // グラフ
-        double xArray[] = makeXArray(0, 10, dx);
+        double xArray[] = makeXArray(min, max, dx);
         GraphDrawer gd = new GraphDrawer(originX, originY, scaleX, scaleY, dx, dy);
         for(int idx = 0; idx < sp.getGraphNum(); ++ idx) {
             gd.addGraph(xArray, sp.calcGraph(idx, xArray));
