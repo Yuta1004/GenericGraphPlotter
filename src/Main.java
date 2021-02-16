1f5d9911a79007f30ffc9eaffac9f7bbdf2db50e
NakagamiYuta <guguru0014@gmail.com>
2021-02-16 16:10:27 +0900

    [update] とりあえずグラフ(らしきもの)が表示されるようになった

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
@@ -12,8 +11,6 @@ import controller.MainUIController;
 
 
 public class Main extends Application {
-
-    private GraphicsContext g;
 
     public static void main(String args[]) {
         launch(args);
@@ -32,160 +29,6 @@ public class Main extends Application {
         stage.show();
     }
 
-    // グラフ描画用
-    /*
-    private int originX, originY, min, max;
-    private double scaleX, scaleY, dx, dy;
-    String script;
-    */
-
-    // GUI部品
-    /*
-    private Scrollbar minScBar, maxScBar, scaleXScBar, dxScBar;
-    private TextArea scriptArea, viewSArea, helpVArea;
-    private Button editBtn, helpBtn;
-    */
-
-    /* コンストラクタ */
-    /*
-    public Main() {
-        originX = 100;
-        originY = 700;
-        scaleX = 80; // 8.5 ~ 80
-        scaleY = 80.0;
-        min = 0;
-        max = 10;  // 10 ~ 100
-        dx = 0.5;
-        dy = 1.0;
-        script = "var a, b, c, d\na = 1.2\nb = -1.2\nc = 1.2\nd = 0.1\n\nplotd << sin(a*x + b) + cos(c*x + d) + 2";
-    }
-    */
-
-    /* init : Applet初期化 */
-    /*
-    public void init() {
-        // GUI初期化(min変更バー)
-        setLayout(null);
-        minScBar = new Scrollbar(Scrollbar.HORIZONTAL, 0, 1, 0, 100);
-        minScBar.setBounds(1050, 70, 310, 20);
-        minScBar.addAdjustmentListener(this);
-        add(minScBar);
-
-        // GUI初期化(max変更バー)
-        maxScBar = new Scrollbar(Scrollbar.HORIZONTAL, 10, 1, 1, 101);
-        maxScBar.setBounds(1050, 170, 310, 20);
-        maxScBar.addAdjustmentListener(this);
-        add(maxScBar);
-
-        // GUI初期化(scaleX変更バー)
-        scaleXScBar = new Scrollbar(Scrollbar.HORIZONTAL, 72, 1, 8, 81);
-        scaleXScBar.setBounds(1050, 270, 310, 20);
-        scaleXScBar.addAdjustmentListener(this);
-        add(scaleXScBar);
-
-        // GUI初期化(dx変更バー)
-        dxScBar = new Scrollbar(Scrollbar.HORIZONTAL, 50, 1, 1, 101);
-        dxScBar.setBounds(1050, 370, 310, 20);
-        dxScBar.addAdjustmentListener(this);
-        add(dxScBar);
-
-        // スクリプト用TextArea
-        scriptArea = new TextArea(script);
-        scriptArea.setBounds(200, 170, 650, 500);
-        scriptArea.setFont(new Font("Monaco", Font.PLAIN, 30));
-        scriptArea.setVisible(false);
-        add(scriptArea);
-
-        // 面積表示用TextArea
-        viewSArea = new TextArea("");
-        viewSArea.setBounds(1050, 470, 310, 220);
-        viewSArea.setFont(new Font("Hannotate", Font.PLAIN, 33));
-        viewSArea.setEditable(false);
-        add(viewSArea);
-
-        // エディタ起動ボタン
-        editBtn = new Button("Edit Script");
-        editBtn.setBounds(1050, 720, 310, 70);
-        editBtn.setFont(new Font("Hannotate", Font.PLAIN, 25));
-        editBtn.addActionListener(this);
-        add(editBtn);
-
-        // ヘルプボタン
-        helpBtn = new Button("?");
-        helpBtn.setBounds(740, 30, 30, 30);
-        helpBtn.setFont(new Font("Hannotate", Font.PLAIN, 20));
-        helpBtn.addActionListener(this);
-        add(helpBtn);
-
-        // ヘルプ表示エリア
-        helpVArea = new TextArea(getHelp());
-        helpVArea.setBounds(130, 100, 800, 650);
-        helpVArea.setFont(new Font("Hannotate", Font.PLAIN, 25));
-        helpVArea.setEditable(false);
-        helpVArea.setVisible(false);
-        add(helpVArea);
-    }
-    */
-
-    /* paint : Applet描画 */
-    /*
-    public void paint(Graphics g) {
-        // 背景
-        g.setColor(new Color(255, 255, 255));
-        g.fillRect(0, 0, getSize().width, getSize().height);
-
-        // スクリプト解析
-        ScriptParser sp = new ScriptParser(script);
-        try{
-            sp.parse();
-        } catch(Exception e) {
-            script = "";
-            repaint();
-        }
-
-        // グラフ & 数値積分
-        int graphNum = sp.getGraphNum();
-        double sList[] = new double[graphNum];
-        double xArray[] = makeXArray(min, max, dx), yArray[];
-        GraphDrawer gd = new GraphDrawer(originX, originY, scaleX, scaleY, dx, dy);
-        for(int idx = 0; idx < graphNum; ++ idx) {
-            yArray = sp.calcGraph(idx, xArray);
-            sList[idx] = numIntegration(dx, yArray);
-            gd.addGraph(idx, xArray, yArray, sp.getVDSetting(idx));
-        }
-        gd.draw((Graphics2D)g);
-
-        // 面積表示
-        String viewSMsg[] = new String[sList.length];
-        for(int idx = 0; idx < sList.length; ++ idx)
-            viewSMsg[idx] =  String.format("%c : %.4f", 'A'+idx, sList[idx]);
-        viewSArea.setText(String.join("\n", viewSMsg));
-
-        // GUI部品の説明
-        g.setColor(new Color(0, 0, 0));
-        g.drawString("Min : " + min, 1050, 50);
-        g.drawString("Max : " + max, 1050, 150);
-        g.drawString("Scale : " + scaleX, 1050, 250);
-        g.drawString("dx : " + dx, 1050, 350);
-        g.drawString("Surface Area", 1050, 450);
-
-        // タイトル
-        g.setFont(new Font("TimesRoman", Font.BOLD, 40));
-        g.drawString("Generic Graph Plotter", 340, 50);
-
-        // TextAreaの枠
-        if(scriptArea.isVisible()) {
-            g.setColor(new Color(50, 100, 255));
-            g.fillRect(190, 160, 670, 520);
-        }
-        if(helpVArea.isVisible()) {
-            g.setColor(new Color(100, 255, 50));
-            g.fillRect(120, 90, 820, 670);
-        }
-    }
-    */
-
-    /* adjustmentValueChanged : GUIイベント受け取り */
     /*
     public void adjustmentValueChanged(AdjustmentEvent e) {
         // 値取得
--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -1,15 +1,89 @@
 package controller;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.control.TextArea;
+
+import parser.ScriptParser;
+import graph.GraphDrawer;
 
 public class MainUIController implements Initializable {
 
+    public final int WIDTH = 1400;
+    public final int HEIGHT = 800;
+
+    private GraphicsContext g;
+    private int originX, originY, min, max;
+    private double scaleX, scaleY, dx;
+    private String script;
+
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private TextArea surfaceView;
+
     @Override
     public void initialize(URL location, ResourceBundle resource) {
+        g = canvas.getGraphicsContext2D();
 
+        originX = 100;
+        originY = 700;
+        scaleX = 80; // 8.5 ~ 80
+        scaleY = 80.0;
+        min = 0;
+        max = 10;  // 10 ~ 100
+        dx = 0.5;
+        script = "var a, b, c, d\na = 1.2\nb = -1.2\nc = 1.2\nd = 0.1\n\nplotd << sin(a*x + b) + cos(c*x + d) + 2";
+
+        draw();
     }
 
+    public void draw() {
+        // 背景
+        g.setFill(Color.WHITE);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        // スクリプト解析
+        ScriptParser sp = new ScriptParser(script);
+        try{
+            sp.parse();
+        } catch(Exception e) {
+            script = "";
+            draw();
+            return;
+        }
+
+        // グラフ & 数値積分
+        int graphNum = sp.getGraphNum();
+        double sList[] = new double[graphNum];
+        double xArray[] = makeXArray(min, max, dx), yArray[];
+        GraphDrawer gd = new GraphDrawer(originX, originY, scaleX, scaleY);
+        for(int idx = 0; idx < graphNum; ++ idx) {
+            yArray = sp.calcGraph(idx, xArray);
+            sList[idx] = Arrays.stream(yArray).sum() * dx;
+            gd.addGraph(idx, xArray, yArray, sp.getVDSetting(idx));
+        }
+        gd.draw(g);
+
+        // 面積表示
+        String viewSMsg[] = new String[sList.length];
+        for(int idx = 0; idx < sList.length; ++ idx)
+            viewSMsg[idx] =  String.format("%c : %.4f", 'A'+idx, sList[idx]);
+        surfaceView.setText(String.join("\n", viewSMsg));
+    }
+
+    private double[] makeXArray(double min, double max, double diff) {
+        double xArray[] = new double[(int)((max - min) / diff)+1];
+        for(int idx = 0; idx < xArray.length; ++ idx) {
+            xArray[idx] = idx * diff + min;
+        }
+        return xArray;
+    }
 }
