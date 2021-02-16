5ba883aae281bd2295305803575f78c93bb073d0
NakagamiYuta <guguru0014@gmail.com>
2021-02-16 22:12:22 +0900

    [add] スクリプトファイルの読み書きを出来るようにした

--- src/controller/EditorController.java
+++ src/controller/EditorController.java
@@ -3,6 +3,9 @@ package controller;
 import java.io.File;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,7 +30,7 @@ public class EditorController implements Initializable {
     @FXML
     private ChoiceBox<String> presetChoice;
     @FXML
-    private Button loadBtn, doneBtn;
+    private Button loadBtn, saveBtn, doneBtn;
 
     public EditorController() {
         presets = new HashMap<String, String>();
@@ -40,48 +43,57 @@ public class EditorController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resource) {
-        loadBtn.setOnAction(event -> {
-            URL scriptPath = getFilePath();
-            if(scriptPath == null) return;
-
-            script += "12044021 ";
-            try {
-                String line;
-                BufferedReader br = new BufferedReader(new InputStreamReader(scriptPath.openStream()));
-                while((line = br.readLine()) != null) {
-                    script += line+"\n";
-                }
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-            script = script.split("12044021")[1];
-            scriptTArea.setText(script);
-        });
-
+        loadBtn.setOnAction(event -> loadScript(getFilePath(false)) );
+        saveBtn.setOnAction(event -> saveScript(getFilePath(true)) );
         doneBtn.setOnAction(event -> doneBtn.getScene().getWindow().hide() );
-
         presetChoice.getItems().addAll(presets.keySet());
         presetChoice.valueProperty().addListener((ov, old_val, new_val) -> {
             script = presets.get(new_val);
             scriptTArea.setText(script);
         });
-
         scriptTArea.setText(script);
         scriptTArea.textProperty().addListener((ov, old_val, new_val) -> script = new_val );
     }
 
-    private URL getFilePath() {
+    private URL getFilePath(boolean doSave) {
         FileChooser chooser = new FileChooser();
         chooser.setTitle("Select script file (*.es or *.txt)");
         chooser.getExtensionFilters().add(new ExtensionFilter("DataFile", "*.es", "*.txt"));
 
-        File file = chooser.showOpenDialog((Stage)loadBtn.getScene().getWindow());
+        File file = doSave ? chooser.showSaveDialog((Stage)loadBtn.getScene().getWindow()) : chooser.showOpenDialog((Stage)loadBtn.getScene().getWindow());
         try {
             return file == null ? new URL("file:///null") : file.toURI().toURL();
         } catch(Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void loadScript(URL scriptURL) {
+        String line;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(scriptURL.openStream()));
+            script = "";
+            while((line = br.readLine()) != null)
+                script += line+"\n";
+            scriptTArea.setText(script);
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
+    private void saveScript(URL saveURL) {
+        // パス変換
+        File saveFile = null;
+        try {
+            saveFile = new File(saveURL.toURI());
+        } catch (Exception e) { return; }
+
+        // 保存
+        try {
+            FileWriter fw = new FileWriter(saveFile.getAbsolutePath(), false);
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+            pw.println(script);
+            pw.close();
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
 }
